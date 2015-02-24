4ab95cb11faa171775315f542ec61ea6b5c35021
Lesha Koss <regiusprod@gmail.com>
2015-02-24 18:05:47 +0600

    Add PostgreSQL support

--- project.clj
+++ project.clj
@@ -4,7 +4,12 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.3.1"]
-                 [ring/ring-defaults "0.1.2"]]
+                 [ring/ring-defaults "0.1.2"]
+                 [cheshire "5.4.0"]
+                 [org.clojure/java.jdbc "0.3.6"]
+                 [postgresql "9.1-901.jdbc4"]
+                 [com.cemerick/friend "0.2.1"]
+                 [friend-oauth2 "0.1.3"]]
   :plugins [[lein-ring "0.8.13"]]
   :ring {:handler information-systems-lab1.handler/app}
   :profiles
--- src/information_systems_lab1/handler.clj
+++ src/information_systems_lab1/handler.clj
@@ -1,10 +1,45 @@
 (ns information-systems-lab1.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [cheshire.core :as json]
+            [clojure.java.jdbc :as db]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [cemerick.friend :as friend]
+            (cemerick.friend [workflows :as workflows]
+                             [credentials :as creds])
+            [friend-oauth2.workflow :as oauth2]
+            [friend-oauth2.util :refer [format-config-uri get-access-token-from-params]]))
+
+(defn get-index []
+  "Hello world")
+
+(defn get-all-documents []
+  (json/generate-string
+    (db/query
+      "postgresql://localhost:5432/my_blog"
+      ["select * from posts"])))
+
+(defn create-new-document []
+  "Create new document")
+
+(defn get-document [id]
+  (str id))
+
+(defn update-document [id body]
+  (str id " " body))
+
+(defn delete-document [id]
+  (str id))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
+  (GET "/" [] (get-index))
+  (context "/api" []
+    (GET  "/" [] (get-all-documents))
+    (POST "/" {body :body} (create-new-document body))
+    (context "/:id" [id]
+      (GET "/" [] (get-document id))
+      (PUT "/" {body :body} (update-document id body))
+      (DELETE "/" [] (delete-document id))))
   (route/not-found "Not Found"))
 
 (def app
